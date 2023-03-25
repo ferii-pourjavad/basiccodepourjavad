99c3fd6405162e6f8431287cf88a08132f1c687c
ferii-pourjavad <128896842+ferii-pourjavad@users.noreply.github.com>
2023-03-25 16:24:36 +0330

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,65 @@
+
+# double_num = 0
+# def double_number(base_number):
+#     base_number=123
+#     base_number = base_number*2
+#     double_num=base_number
+#     print(base_number)
+# double_number('base_number')
+#
+
+# def greet(welcoming):
+#       a1 = "zahra"
+#
+#       print("welcome    " + str (a1))
+# greet('welcoming')
+
+
+
+
+
+# typo = "chinchill"
+# def append_a(name):
+#  name = name+"a"
+#  return name
+#  print(append_a(typo))
+#
+#
+
+
+num1 = input('Enter first number:- ')
+num2 = input('Enter second number:- ')
+
+sum = float(num1) + float(num2)
+
+print('The sum is', sum)
+
+# NumList = []
+# evenSum = 0
+#
+# Number = int(input("Please enter the total number of list elements: "))
+# for i in range(1, Number + 1):
+#  value = int(input("Please enter the value of %d Elements: " % i))
+#
+# NumList.append(value)
+# for j in range(Number):
+#  if (NumList[j] % 2 == 0):
+#   evenSum = evenSum + NumList[j]
+# print("\n The sum of even numbers in the list= ", evenSum)
+
+
+# def(factorial)
+#  neumeriaca(adjuatment)
+#  format(factoraial)
+#  for i in range (number):
+#   if (factorial <1 ,%2==1)
+#    else(factorial>2,/2==1)
+
+
+
+
+
+
+
+
+
