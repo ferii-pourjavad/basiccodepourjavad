ae4fa851ba08dc7b04fc420db01545587b65f3e7
ferii-pourjavad <128896842+ferii-pourjavad@users.noreply.github.com>
2023-03-25 16:00:28 +0330

    Add files via upload

---
+++ double.py
@@ -0,0 +1,19 @@
+
+# double_num = 0
+# def double_number(base_number):
+#     base_number=123
+#     base_number = base_number*2
+#     double_num=base_number
+#     print(base_number)
+# double_number('base_number')
+#
+
+def greet(welcoming):
+      a1 = "zahra"
+
+      print("welcome    " + str (a1))
+greet('welcoming')
+
+
+
+
---
+++ welcome.py
@@ -0,0 +1,39 @@
+
+# double_num = 0
+# def double_number(base_number):
+#     base_number=123
+#     base_number = base_number*2
+#     double_num=base_number
+#     print(base_number)
+# double_number('base_number')
+#
+
+# def greet(welcoming):
+#       a1 = "zahra"
+#
+#       print("welcome    " + str (a1))
+# greet('welcoming')
+
+
+
+
+
+# typo = "chinchill"
+# def append_a(name):
+#  name = name+"a"
+#  return name
+#  print(append_a(typo))
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
